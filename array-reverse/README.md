ffad026a9b50d20b152d649e71eebf3f13ca76f7
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-06-17 20:19:02 +0300

    creating reverse

---
+++ array-reverse/README.md
@@ -0,0 +1,7 @@
+# data-structures-and-algorithms
+
+## Reveres Array
+
+![1](../array-reverse/Untitled.jpg)
+
+![2](../array-reverse//Untitled%20(1).jpg)
---
+++ array-reverse/array-reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace array_reverse
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] Array = new int[] {1,2,3,4};
+			Array = Reverse(Array);
+			for(int i=0; i<Array.Length; i++)
+			{
+				Console.WriteLine(Array[i]);	
+			}	
+			
+		}
+
+		static int[] Reverse(int[] arr)
+		{
+			if (arr == null)
+			{
+				return null;
+			}
+
+			else
+			{
+				int[] newArr = new int[arr.Length];
+				int Index = 0;
+				for(int i=arr.Length-1; i>=0; i--)
+				{
+					newArr[Index] = arr[i];
+					Index++;	
+				}
+				return newArr;
+			}
+		}
+	}
+}
